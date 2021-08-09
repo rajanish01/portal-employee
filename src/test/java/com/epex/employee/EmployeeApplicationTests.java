23c67c935c19e0adb4a0fabfcf8c0376c84253c3
Rajanish <rajanishatwork@gmail.com>
2021-08-10 03:06:00 +0530

    initial commit
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

---
+++ src/test/java/com/epex/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epex.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
